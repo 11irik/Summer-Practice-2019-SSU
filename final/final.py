9a942a94763b748649b8b68405ae741e2e464a7f
Kirill <lukashinkv@gmail.com>
2019-10-02 01:35:48 +0400

    Example of formating the source table

--- final/final.py
+++ final/final.py
@@ -1,36 +1,36 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import random
-import datetime
-import calendar
-from collections import Counter
 import numpy as np
 
 
-df = pd.read_csv('FinalBase.csv', sep=';',  low_memory=False)
-df.fillna(0, inplace=True)
-df = df.drop(df[df['Schengen State'] == 0].index)
 
-#task 2
+# df = pd.read_csv('FinalBase.csv', sep=';',  low_memory=False)
+# df.fillna(0, inplace=True)
+# df = df.drop(df[df['Schengen State'] == 0].index)
+# df = df.drop(df.columns[0], axis=1)
+# df = df.sample(frac = 1)
+
+df = pd.read_csv('drop.csv')
+
+#df.to_csv('drop.csv')
+
 column = 'Uniform visas applied for'
 
+# Task 2
+print('Task 2:')
 print('Median: ', df[column].median())
-
-
 print('Mode: ', df.mode()[column][0])
-#print('????: ', Counter(df[column]).most_common())
-print(df[df[column] == 16])
-
 print('Average: ', df[column].mean())
 print('Minimum: ', df[column].min())
 print('Maximum: ', df[column].max())
 
-# task 3
-max = df[column].max()
-df['delta'] = abs(df[column] - max)
+# Task 3
+print('Task 3:')
+maxVisas = df[column].max()
+df['delta'] = abs(df[column] - maxVisas)
 print(df.loc[:, 'delta'], sep='\n')
 
-# task 4
+# Tasks 4, 5
 font = {'size': 15}
 plt.rc('font', **font)
 groups = df.groupby('Schengen State').size()
@@ -38,57 +38,34 @@ plt.pie = groups.plot(kind='pie', figsize=(15, 15), autopct='%.2f',
      title='Number of visas to the country', legend=False, subplots=True)
 plt.show()
 
-
-
-
-
-
-
-consulate = list(set(df.loc[df['Schengen State'] == 'Austria', 'Consulate'].values))
-print(consulate)
-print(len(consulate))
-
-cons = list(df.loc[df['Schengen State'] == 'Austria', column].values)
-
-print(cons)
-print(len(cons))
-
-x = np.arange(len(consulate))
-#
+# Task 6
 font = {'size': 7}
 plt.rc('font', **font)
 fig, ax = plt.subplots()
-plt.bar(x[:5], cons[:5])
-plt.xticks(x[:5], consulate[:5])
 
-plt.show()
+def generateChart(country):
+     consulate = list(set(df.loc[df['Schengen State'] == country, 'Consulate'].values))
+     consulates = list(df.loc[df['Schengen State'] == country, column].values)
+     x = np.arange(len(consulate))
+     plt.bar(x[:5], consulates[:5])
+     plt.xticks(x[:5], consulate[:5])
+     plt.show()
 
+generateChart('Belgium')
+generateChart('Austria')
+generateChart('Finland')
 
+# Task 7
+print('Task 7:')
+groups = []
+names = []
+for key, groupdf in df.groupby('Schengen State'):
+     groups.append(groupdf)
+     names.append(key)
 
+groups[4] = groups[4].sort_values(by=['Uniform visas applied for', 'Consulate'])
+groups[6] = groups[6].sort_values(by=['Total ATVs and uniform visas not issued', 'Country where consulate is located'])
+print(names[4], '\n', groups[4][['Uniform visas applied for', 'Consulate']])
+print(names[6], '\n', groups[6][['Total ATVs and uniform visas not issued', 'Country where consulate is located']])
 
-
-
-
-
-
-# 6 ? ??????????? ?? ???????????? ?????? ??????
-# ??? ??????? ????? ????????? ?????? (???????????)
-# countries = list(set(df.loc[:, 'Schengen State'].values))
-#
-# res = []
-# for x in countries:
-#     res.append(df.loc[df['Schengen State'] == x, column])
-# colors = ['#cc0605', '#009900', '#0000FF', '#FFFFFF', '#FF33FF',
-#           '#CCFF00', '#666666']
-# fig, ax = plt.subplots()
-
-# for i in range(len(res)):
-#     x_axis = np.full(len(res[i]), i+1)
-#     ax.scatter(x_axis, res[i].values, c=colors[i], s=1, label=countries[i])
-# ax.set_facecolor('black')
-# ax.set_title('????????????? ?????????? ??? ?? ???????')
-# fig.set_figwidth(5)
-# fig.set_figheight(5)
-# plt.legend(loc='upper left', markerscale=10)
-# plt.show()
 
